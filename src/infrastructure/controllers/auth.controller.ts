c760ebf1f75e83af360c270dd736563bdbd70746
Cesar-Marcano <marcanocesar86@gmail.com>
2025-03-23 15:54:47 -0400

    feat(auth): add email verification and 2FA routes to authentication controller

--- src/infrastructure/controllers/auth.controller.ts
+++ src/infrastructure/controllers/auth.controller.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from 'express'
 
 export class AuthController {
-  public static async login(req: Request, res: Response): Promise<void> {}
   public static async register(req: Request, res: Response): Promise<void> {}
+
+  public static async login(req: Request, res: Response): Promise<void> {}
+
+  public static async verifyEmail(req: Request, res: Response): Promise<void> {}
+
+  public static async enable2FA(req: Request, res: Response): Promise<void> {}
+
+  public static async verify2FA(req: Request, res: Response): Promise<void> {}
+
+  public static async disable2FA(req: Request, res: Response): Promise<void> {}
 }
--- src/infrastructure/routes/auth.route.ts
+++ src/infrastructure/routes/auth.route.ts
@@ -4,4 +4,13 @@ import { AuthController } from '../controllers/auth.controller'
 export const authRouter = Router()
 
 authRouter.post('/login', AuthController.login)
+
 authRouter.post('/register', AuthController.register)
+
+authRouter.get('/verify/:token', AuthController.verifyEmail)
+
+authRouter.post('/2fa/enable', AuthController.enable2FA)
+
+authRouter.post('/2fa/verify', AuthController.verify2FA)
+
+authRouter.post('/2fa/disable', AuthController.disable2FA)
