94882790a8dc0c8dbf3fe7b609e35a3a9a6fb5ae
Cesar-Marcano <marcanocesar86@gmail.com>
2025-03-23 19:11:03 -0400

    feat(repository): update createUser method to return user without password

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,7 +1,10 @@
 import { UserEntity } from '../entities/user.entity'
 
 export interface IUserRepository {
-  createUser(email: string, password: string): Promise<UserEntity>
+  createUser(
+    email: string,
+    password: string,
+  ): Promise<Omit<UserEntity, 'password'>>
   findUserByEmail(email: string): Promise<UserEntity>
   findUserByUsername(username: string): Promise<UserEntity>
 }
--- src/infrastructure/repositories/user.repository.ts
+++ src/infrastructure/repositories/user.repository.ts
@@ -1,10 +1,28 @@
+import { PrismaClient } from '@prisma/client'
 import { UserEntity } from '../../domain/entities/user.entity'
 import { IUserRepository } from '../../domain/repositories/user.repository'
+import { DatabaseConfig } from '../config/database.config'
 
 export class UserRepository implements IUserRepository {
-  createUser(email: string, password: string): Promise<UserEntity> {
-    throw new Error('Method not implemented.')
+  private prisma: PrismaClient
+
+  constructor(databaseConfig: DatabaseConfig) {
+    this.prisma = databaseConfig.getClient()
   }
+
+  async createUser(
+    email: string,
+    password: string,
+  ): Promise<Omit<UserEntity, 'password'>> {
+    const newUser = await this.prisma.user.create({
+      data: UserEntity.create(email, password),
+    })
+
+    const user = UserEntity.fromPrisma(newUser).toPublicEntity()
+
+    return user
+  }
+
   findUserByEmail(email: string): Promise<UserEntity> {
     throw new Error('Method not implemented.')
   }
